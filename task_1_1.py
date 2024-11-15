9292c25559b0091a431d5c6eeb1d150ffc8ad558
Anatoliy-Bor <89204220661@mail.ru>
2024-11-15 18:28:27 +0300

    Add files via upload

---
+++ task_1_1.py
@@ -0,0 +1,14 @@
+
+print("1st program")
+print((9 ** 0.5) * 5)
+print("2nd program")
+print(9.99 > 9.98 and 1000 != 1000.1)
+print("3rd program")
+a = 2 * 2 + 2
+print(a)
+b = 2 * (2 + 2)
+print(b)
+print(a == b)
+print("4th program")
+c = float('123.456')
+print(int(c *10) % 10)
